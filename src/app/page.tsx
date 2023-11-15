647758e20fd5cf90c3c1c1b88961ea351410aa0e
Juan Gomes <89032763+juamgomes@users.noreply.github.com>
2023-11-15 00:39:10 -0400

    Mudei cor de fundo e tamanho na div de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className='bg-refu bg-cover h-screen flex justify-center p-10'>
          
-        <div className='p-10 bg-slate-900 rounded-lg h-80 lg:h-2/3 w-4/5'>
+        <div className='p-10 bg-slate-900 rounded-lg h-80 lg:h-60 w-4/5'>
           <h1 className='font-bold text-2xl'>Tudo que você precisa em um só lugar!</h1>
           <h2 className='text-slate-400'>Ajudamos voçê com a sua regularização</h2>
           <button className='mt-10 bg-cyan-600 py-4 px-6 rounded-lg' onClick={() => router.push('/dashboard')}>Começar</button>
